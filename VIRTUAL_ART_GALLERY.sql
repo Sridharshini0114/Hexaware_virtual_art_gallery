22c01623b74b98a1c0d295b1b0878ba55cc8812d
Sridharshini0114 <144587121+Sridharshini0114@users.noreply.github.com>
2024-11-11 12:01:23 +0530

    Update VIRTUAL_ART_GALLERY.sql

--- VIRTUAL_ART_GALLERY.sql
+++ VIRTUAL_ART_GALLERY.sql
@@ -61,7 +61,8 @@ INSERT INTO Artworks (Title, ArtistID, CategoryID, Year, Description, ImageURL) 
 -- INSERTION OF EXHIBITIONS TABLE
 INSERT INTO Exhibitions (Title, StartDate, EndDate, Description) VALUES
  ('Modern Art Masterpieces', '2023-01-01', '2023-03-01', 'A collection of modern art masterpieces.'),
- ('Renaissance Art', '2023-04-01', '2023-06-01', 'A showcase of Renaissance art treasures.');
+ ('Renaissance Art', '2023-04-01', '2023-06-01', 'A showcase of Renaissance art treasures.');
+
 
  --CREATION OF EXHIBITION ARTWORKS
  CREATE TABLE ExhibitionArtworks (
@@ -85,7 +86,8 @@ INSERT INTO Exhibitions (Title, StartDate, EndDate, Description) VALUES
 	 SELECT *FROM Exhibitions
 ----------------------------------------------------------
 --1. Retrieve the names of all artists along with the number of artworks they have in the gallery, and
---list them in descending order of the number of artworks.SELECT a.Name AS ArtistName, COUNT(ar.ArtworkID) AS ArtworkCount
+--list them in descending order of the number of artworks.
+SELECT a.Name AS ArtistName, COUNT(ar.ArtworkID) AS ArtworkCount
 FROM Artists as a
 JOIN Artworks  as ar ON a.ArtistID = ar.ArtistID
 GROUP BY a.Name
@@ -93,8 +95,10 @@ ORDER BY ArtworkCount DESC;
 
 
 --2.List the titles of artworks created by artists from 'Spanish' and 'Dutch' nationalities, and order
---them by the year in ascending order.SELECT *FROM Artists
-  SELECT *FROM Artworksselect a. title,a.year
+--them by the year in ascending order.
+SELECT *FROM Artists
+  SELECT *FROM Artworks
+select a. title,a.year
 from artworks as a
 join Artists as ar on ar.ArtistID=a.ArtistID
 where ar.Nationality in ('spanish','dutch')
@@ -111,7 +115,8 @@ GROUP BY a.Name
 
 
 --4.List the names of artworks from the 'Modern Art Masterpieces' exhibition, along with their
---artists and categories. select 
+--artists and categories.
+ select 
  ar.Title,a.Name,c.Name
  from ExhibitionArtworks ea
  join Artworks ar on ea.ArtworkID=ar.ArtworkID
@@ -120,7 +125,9 @@ GROUP BY a.Name
  join Exhibitions e on e.ExhibitionID=e.ExhibitionID
  where e.title='Modern Art Masterpieces'
 
---5.Find the artists who have more than two artworks in the gallery select a.Name 
+
+--5.Find the artists who have more than two artworks in the gallery
+ select a.Name 
  from artists a
  join Artworks ar
  on a.ArtistID=ar.ArtistID
@@ -158,7 +165,17 @@ group by
 a.Name 
 having count(ar.ArtworkID)>3
 
---9.Find the artworks created by artists from a specific nationality (e.g., Spanish).select ar.title from Artworks arjoin Artists as a on ar.ArtistID=a.ArtistIDjoin Categories as c on ar.CategoryID=c.CategoryIDwhere a.Nationality='spanish'--10. List exhibitions that feature artwork by both Vincent van Gogh and Leonardo da Vinci.select e.Title
+--9.Find the artworks created by artists from a specific nationality (e.g., Spanish).
+select ar.title 
+from Artworks ar
+join Artists as a on ar.ArtistID=a.ArtistID
+join Categories as c on ar.CategoryID=c.CategoryID
+where a.Nationality='spanish'
+
+
+--10. List exhibitions that feature artwork by both Vincent van Gogh and Leonardo da Vinci.
+
+select e.Title
 from Exhibitions e
 join ExhibitionArtworks ea on e.ExhibitionID=ea.ExhibitionID
 join Artworks ar on ea.ArtworkID=ar.ArtworkID
@@ -166,13 +183,17 @@ join Artists a on ar.artistId =a.ArtistID
 where a.Name='Vincent van Gogh' or a.Name='Leonardo da Vinci'
 group by e.title 
 having count(distinct a.Name)=2;
-
 
---11. Find all the artworks that have not been included in any exhibition.select ar.Title
+
+
+--11. Find all the artworks that have not been included in any exhibition.
+select ar.Title
 from Artworks ar
 left join ExhibitionArtworks ea on ar.ArtworkID=ea.ArtworkID
-where ea.ExhibitionID is null;
---12.List artists who have created artworks in all available categories.select a.name as artistname
+where ea.ExhibitionID is null;
+
+--12.List artists who have created artworks in all available categories.
+select a.name as artistname
 from artists as a
 join artworks as ar on a.artistid=ar.ArtistID
 join Categories as c on ar.CategoryID=c.CategoryID
@@ -194,28 +215,45 @@ group by a.Name
 having count(ar.artworkId)>2;
 
 --15.List the categories with the average year of artworks they contain, only for categories with more
---than 1 artwork.select c.Name,avg(ar.year) as [Year]
+--than 1 artwork.
+select c.Name,avg(ar.year) as [Year]
 from categories c
 join Artworks ar
 on c.CategoryID=ar.CategoryID
 group by c.Name
-having count(ar.artWorkId)>1;--16.Find the artworks that were exhibited in the 'Modern Art Masterpieces' exhibition.select ar.Title
+having count(ar.artWorkId)>1;
+
+
+--16.Find the artworks that were exhibited in the 'Modern Art Masterpieces' exhibition.
+select ar.Title
 from ExhibitionArtworks ea
 join Artworks ar
 on ea.ArtworkID=ar.ArtworkID
 join Exhibitions e on e.ExhibitionID=e.ExhibitionID
 where e.Title='Modern Art Masterpieces'
 
---17.Find the categories where the average year of artworks is greater than the average year of all
---artworks.select c.Name
+
+--17.Find the categories where the average year of artworks is greater than the average year of all
+--artworks.
+select c.Name
 from Categories c
 join Artworks ar on c.CategoryID=ar.CategoryID
 group by c.Name
-having avg(ar.year)>(select avg(year) from Artworks);--18.. List the artworks that were not exhibited in any exhibition.select ar.Title
+having avg(ar.year)>(select avg(year) from Artworks);
+
+
+
+--18.. List the artworks that were not exhibited in any exhibition.
+select ar.Title
 from Artworks ar
 left join ExhibitionArtworks ea
 on ea.ArtworkID=ar.ArtworkID
-where ea.ExhibitionID is null;--19.Show artists who have artworks in the same category as "Mona Lisa."
+where ea.ExhibitionID is null;
+
+
+
+--19.Show artists who have artworks in the same category as "Mona Lisa."
+
 select a.Name
 from artists a
 join Artworks ar on a.ArtistID =ar.ArtistID
@@ -229,8 +267,12 @@ from Artists a
 left join 
 Artists ar on a.ArtistID=ar.ArtistID
 group by a.Name
-
 
-
-
-
+
+
+
+
+
+
+
+
